28c18f07483c7a7832f5a796a2cdae4c389f20c6
MSKatKing <mskatking08@gmail.com>
2025-05-16 16:55:57 -0500

    Log interrupts and cpu resets for debugging

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ mkdir -p esp/efi/boot
 cp target/x86_64-unknown-uefi/debug/uefi_loader.efi ./esp/efi/boot/bootx64.efi
 cp target/x86_64-unknown-none/debug/kernel ./esp/kernel.elf
 
-qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp
+qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp -d int,cpu_reset -D qemu.log

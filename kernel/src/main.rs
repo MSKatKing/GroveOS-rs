1d9de29336af5d39e016723fd2c0deb67fe3bc0b
MSKatKing <mskatking08@gmail.com>
2025-05-16 00:08:42 -0500

    Link kernel crate with custom linker script

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,6 +3,11 @@
 
 use core::panic::PanicInfo;
 
+unsafe extern "C" {
+    static __kernel_vstart: *const u64;
+    static __kernel_vend: *const u64;
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     loop {}

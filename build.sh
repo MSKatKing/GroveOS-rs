b2948e8bd12b5498306aea614247da82a06e4296
MSKatKing <mskatking08@gmail.com>
2025-05-15 23:26:50 -0500

    Create kernel crate

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ pushd kernel
 cargo build --target x86_64-unknown-none
 popd
 
-mkdir esp\efi\boot
-xcopy .\target\x86_64-unknown-uefi\debug\uefi_loader.efi .\esp\efi\boot\bootx64.efi /y
-xcopy .\target\x86_64-unknown-none\debug\kernel .\esp\kernel.elf /y
+mkdir -p esp/efi/boot
+cp target/x86_64-unknown-uefi/debug/uefi_loader.efi ./esp/efi/boot/bootx64.efi
+cp target/x86_64-unknown-none/debug/kernel ./esp/kernel.elf
 
 qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp

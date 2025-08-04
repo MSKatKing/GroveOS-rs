f88237f02199a49f75e3cdb547eadb65502d70d9
Mark <mskatking08@gmail.com>
2025-08-03 23:21:00 -0500

    Refactor file api

--- kernel/src/io/mod.rs
+++ kernel/src/io/mod.rs
@@ -1,14 +1,49 @@
-use crate::io::ata::{ATA_PIO_DEVICE_PRIMARY, ATA_PIO_DEVICE_SECONDARY};
+use alloc::string::String;
+use alloc::vec::Vec;
 
 mod ata;
 
-const DEVICES: &[&'static dyn DiskDevice] = &[&ATA_PIO_DEVICE_PRIMARY, &ATA_PIO_DEVICE_SECONDARY];
-
-pub trait DiskDevice {
-    fn read_sector(&self, lba: u64, buffer: &mut [u8]) -> Result<(), DiskError>;
-    fn write_sector(&self, lba: u64, buffer: &[u8]) -> Result<(), DiskError>;
+#[derive(Eq, PartialEq)]
+enum FileKind {
+    File,
+    Directory,
 }
 
-pub enum DiskError {
-    NotSupported,
+pub struct File {
+    kind: FileKind,
+    path: String,
+    size: u32,
+    data: Option<Vec<u8>>,
+    fs: &'static dyn FileSystem,
+    start_cluster: u32,
+}
+
+impl File {
+    pub fn open(path: &str) -> Option<Self> {
+        todo!()
+    }
+    
+    pub fn read(&mut self) -> &[u8] {
+        todo!()
+    }
+    
+    pub fn list_children(&self) -> Vec<File> {
+        todo!()
+    }
+    
+    pub fn is_directory(&self) -> bool {
+        self.kind == FileKind::Directory
+    }
+    
+    pub fn filename(&self) -> &str {
+        let trimmed = self.path.trim_end_matches('/');
+        trimmed.rsplit('/').next().unwrap_or("")
+    }
+}
+
+trait FileSystem {
+    fn open(&self, path: &str) -> Option<File>;
+    fn read_file(&self, file: &mut File) -> Option<&[u8]>;
+    fn list_dir(&self, dir: &File) -> Vec<File>;
+    fn root(&self) -> File;
 }

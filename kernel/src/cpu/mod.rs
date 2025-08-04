8b83d393d17453d0af7ec8af6f48b2e3c58e5b3e
Mark <mskatking08@gmail.com>
2025-08-03 23:33:19 -0500

    Implement reading from the disk using ata in pio mode

--- kernel/src/cpu/mod.rs
+++ kernel/src/cpu/mod.rs
@@ -1,2 +1,36 @@
+use core::arch::asm;
+
 pub mod idt;
 pub mod gdt;
+
+#[inline(always)]
+pub fn outb(port: u16, data: u8) {
+    unsafe {
+        asm!("out dx, al", in("dx") port, in("al") data, options(nomem, nostack));
+    }
+}
+
+#[inline(always)]
+pub fn inb(port: u16) -> u8 {
+    let out;
+    unsafe {
+        asm!("in al, dx", out("al") out, in("dx") port, options(nomem, nostack));
+    }
+    out
+}
+
+#[inline(always)]
+pub fn inw(port: u16) -> u16 {
+    let out;
+    unsafe {
+        asm!("in ax, dx", out("ax") out, in("dx") port, options(nomem, nostack));
+    }
+    out
+}
+
+#[inline(always)]
+pub fn io_wait() {
+    for _ in 0..10 {
+        outb(0x80, 0);
+    }
+}
--- kernel/src/io/ata.rs
+++ kernel/src/io/ata.rs
@@ -1,29 +1,56 @@
-use crate::io::{DiskDevice, DiskError};
+use crate::cpu;
 
-pub(super) const ATA_PIO_DEVICE_PRIMARY: AtaPioDevice = AtaPioDevice {
-    io_base: 0x1F0,
-    control_base: 0x3F6,
-    master: true,
-};
+const ATA_PRIMARY_IO: u16 = 0x1F0;
+const ATA_REG_DATA: u16 = 0x00;
+const ATA_REG_ERROR: u16 = 0x01;
+const ATA_REG_SECCOUNT0: u16 = 0x02;
+const ATA_REG_LBA0: u16 = 0x03;
+const ATA_REG_LBA1: u16 = 0x04;
+const ATA_REG_LBA2: u16 = 0x05;
+const ATA_REG_HDDEVSEL: u16 = 0x06;
+const ATA_REG_COMMAND: u16 = 0x07;
+const ATA_REG_STATUS: u16 = 0x07;
+const ATA_CMD_READ_PIO: u8 = 0x20;
+const ATA_SR_BSY: u8 = 0x80;
+const ATA_SR_DRQ: u8 = 0x08;
 
-pub(super) const ATA_PIO_DEVICE_SECONDARY: AtaPioDevice = AtaPioDevice {
-    io_base: 0x170,
-    control_base: 0x376,
-    master: false,
-};
-
-pub struct AtaPioDevice {
-    pub io_base: u16,
-    pub control_base: u16,
-    pub master: bool,
+fn read_status() -> u8 {
+    cpu::inb(ATA_PRIMARY_IO + ATA_REG_STATUS)
 }
 
-impl DiskDevice for AtaPioDevice {
-    fn read_sector(&self, lba: u64, buffer: &mut [u8]) -> Result<(), DiskError> {
-        todo!()
-    }
+fn ata_wait_bsy() {
+    while read_status() & ATA_SR_BSY != 0 { }
+}
 
-    fn write_sector(&self, lba: u64, buffer: &[u8]) -> Result<(), DiskError> {
-        todo!()
+fn ata_wait_drq() {
+    while read_status() & ATA_SR_DRQ != 0 { }
+}
+
+pub fn ata_read_sector(lba: u32, buffer: &mut [u8]) {
+    ata_wait_bsy();
+    
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_SECCOUNT0, 1);
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_LBA0, lba as u8);
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_LBA1, (lba >> 8) as u8);
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_LBA2, (lba >> 16) as u8);
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_HDDEVSEL, 0xE0 | ((lba >> 24) & 0x0F) as u8);
+    cpu::outb(ATA_PRIMARY_IO + ATA_REG_COMMAND, ATA_CMD_READ_PIO);
+    
+    ata_wait_bsy();
+    ata_wait_drq();
+    
+    let buffer = buffer.as_mut_ptr().cast::<u16>();
+    for i in 0..256 {
+        unsafe {
+            core::ptr::write(buffer.add(i), cpu::inw(ATA_PRIMARY_IO + ATA_REG_DATA));
+        }
+    }
+    
+    cpu::io_wait();
+}
+
+pub fn ata_read_sectors(lba: u32, buffer: &mut [u8]) {
+    for (idx, chunk) in buffer.chunks_exact_mut(512).enumerate() {
+        ata_read_sector(lba + idx as u32, chunk);
     }
 }
